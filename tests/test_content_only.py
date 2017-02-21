a04aa0cc36924806d2ab0c84bb3d39f8f0bad521
yaoyanan <yaoyanan@kloudpeak.com>
2017-02-21 11:34:09 +0800

    update test

--- tests/test_content_only.py
+++ tests/test_content_only.py
@@ -4,8 +4,8 @@ from tidypage import Document
 
 HTMLS = os.path.join(os.path.dirname(__file__), 'htmls')
 
-class TestArticleContent(unittest.TestCase):
-    def test_best_elem_is_root_and_passing(self):
+class TestContentOnly(unittest.TestCase):
+    def test_content_parser(self):
         html = open(os.path.join(HTMLS, "aleyna-tilki-basrol-oynamaya-hazirlaniyor.html")).read()
         doc = Document(html, True, "http://www.test.com/test.html")
         print doc.content()
